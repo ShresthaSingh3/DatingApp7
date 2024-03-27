e9a9e7a38aabe40e8bdaeb329457a43ccb88ccd4
Shrestha . <shrestha3@deloitte.com>
2024-03-27 16:07:56 +0530

    code changes for end of section10

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -5,10 +5,5 @@ export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = 
   if(component.editForm?.dirty) {
     return confirm('Are you sure you want to continue? Any unsaved changes will be lost')
   }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> ff13ddfc743b9c657a137e716d8dff0724be10d7
   return true;
 };
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -13,33 +13,19 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit{
-<<<<<<< HEAD
   @ViewChild('editForm') editForm:  NgForm | undefined;
   @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any){
     if(this.editForm?.dirty) {
-=======
-  @ViewChild('editForm') editForm: NgForm | undefined;
-  @HostListener('window:beforeunload',['$event']) unloadNotification($event: any) {
-    if (this.editForm?.dirty) {
->>>>>>> ff13ddfc743b9c657a137e716d8dff0724be10d7
       $event.returnValue = true;
     }
   }
   member: Member | undefined;
   user: User | null = null;
-<<<<<<< HEAD
-  
+
   constructor(private accountService: AccountService, private memberService: MembersService, 
     private toastr: ToastrService) { 
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => this.user =user
-=======
-
-  constructor(private accountService: AccountService, private memberService: MembersService, 
-    private toastr: ToastrService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => this.user = user
->>>>>>> ff13ddfc743b9c657a137e716d8dff0724be10d7
     })
   }
 
@@ -56,18 +42,10 @@ export class MemberEditComponent implements OnInit{
 
   updateMember() {
     this.memberService.updateMember(this.editForm?.value).subscribe({
-<<<<<<< HEAD
       next: _ => {
-=======
-      next: _=> {
->>>>>>> ff13ddfc743b9c657a137e716d8dff0724be10d7
         this.toastr.success('Profile updated successfully');
         this.editForm?.reset(this.member);
       }
     })
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ff13ddfc743b9c657a137e716d8dff0724be10d7
